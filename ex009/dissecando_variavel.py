71b5f6cb8236d50c417493348b06a8b206cadd7e
Thiago Matheus da Silva Mares <148148163+thiagosilva80@users.noreply.github.com>
2026-07-28 19:11:56 -0300

    ...

---
+++ ex009/dissecando_variavel.py
@@ -0,0 +1,14 @@
+a = input("Digite algo: ")
+print('O tipo primitivo desse valor é: ', type(a))
+
+print("Só tem espaço? ", a.isspace())
+
+print("È um numero? ", a.isnumeric())
+
+print('È alfabetico? ',a.isalpha())
+
+print('É um numero? ', a.isalnum())
+
+print('È está em maiusculas? ' , a.isupper)
+
+print ('É minuscula? ', a.islower)
